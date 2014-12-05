5eee16d1335874cb10dd7b9f2aeddba5698eaa96
tsschnoc <thomas.schnocklake@gmail.com>
2014-12-05 08:51:20 +0100

    Update favicon.js

--- favicon.js
+++ favicon.js
@@ -1,6 +1,8 @@
 function setTextFavicon() {
     var favIconUrl;
-    var text = 'SF';
+    var text = '?';
+    var backgroundcolor = 'WHITE';
+    var textcolor = 'BLACK';
     
     var oid;
     var host;
@@ -14,53 +16,59 @@ function setTextFavicon() {
         oid = SfdcDevConsole.SID.substring(0,15);    
         host = '';
     }
-    
+    if (host === 'login.salesforce.com') {
+        backgroundcolor = 'RED';
+        textcolor = 'BLACK';
+    }
+    if (host === 'test.salesforce.com') {
+        backgroundcolor = 'BLUE';
+        textcolor = 'WHITE';
+    }
+
     
     switch (oid) {
-      case '00D19000000Diqk':
-        text = 'EM';
-        break;
-      case '00DW0000003idiy':
-        text = 'FA';
-        break;
+        case '00D19000000Diqk':
+            text = 'EM';
+            break;
+        case '00DW0000003idiy':
+            text = 'FA';
+            break;
+            
+            //parxch live
+        case '00D200000001dcm':
+            text = 'PX';
+            backgroundcolor = 'GRAY';
+            textcolor = 'WHITE';
+            break;
     }
 
     if (text) {
-      canvas = document.createElement('canvas');
-      canvas.width = 20
-      canvas.height = 20
-      context = canvas.getContext('2d');
+        canvas = document.createElement('canvas');
+        canvas.width = 20
+        canvas.height = 20
+        context = canvas.getContext('2d');
 
-      if (host === 'login.salesforce.com') {
         context.rect(0,0,20,20);
-        context.fillStyle="blue";
-        context.fill();       
-              context.fillStyle = 'WHITE';
-
-      }
-      
-      if (host === 'test.salesforce.com') {
-        context.rect(0,0,20,20);
-        context.fillStyle="red";
+        context.fillStyle=backgroundcolor;
         context.fill();     
-              context.fillStyle = 'BLACK';
+        context.fillStyle=textcolor;
 
-      }
+     
+        
+        context.font = "15px Georgia";
+        context.fillText(text, 0, 17);
+        
+        favIconUrl = canvas.toDataURL('image/png');
+        
 
-      context.font = "15px Georgia";
-      context.fillText(text, 0, 17);
-
-      favIconUrl = canvas.toDataURL('image/png');
-
-
-      var link = document.querySelector("link[rel~='icon'], link[rel~='ICON']");
-      if (!link) {
-        link = document.createElement("link");
-        link.setAttribute("rel", "icon");
-        link.type = "image/x-icon";
-        document.head.appendChild(link);
-      }
-      link.href = favIconUrl;
+        var link = document.querySelector("link[rel~='icon'], link[rel~='ICON']");
+        if (!link) {
+            link = document.createElement("link");
+            link.setAttribute("rel", "icon");
+            link.type = "image/x-icon";
+            document.head.appendChild(link);
+        }
+        link.href = favIconUrl;
     }
   }
   setTextFavicon();
